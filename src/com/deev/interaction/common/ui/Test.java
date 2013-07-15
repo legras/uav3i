c7c47268bfe76361e82552fc114d9b99cbd329d0
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2013-07-15 18:26:05 +0200

    Test.

--- src/com/deev/interaction/common/ui/Test.java
+++ src/com/deev/interaction/common/ui/Test.java
@@ -3,7 +3,8 @@ package com.deev.interaction.common.ui;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	  
+		// Un petit commentaire (de qualité) ajouté par Philippe TANGUY... 
 
 	}
 
