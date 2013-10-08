dfb85b3a3b7be464efc2aeaf9ebb3f376c1322cd
phtanguy <philippe.tanguy@telecom-bretagne.eu>
2013-10-08 19:19:05 +0200

    Modifs UAV3iSettings.java

--- src/eu/telecom_bretagne/uav3i/UAV3iSettings.java
+++ src/eu/telecom_bretagne/uav3i/UAV3iSettings.java
@@ -6,49 +6,47 @@ import java.util.Properties;
 
 public class UAV3iSettings
 {
-	//-----------------------------------------------------------------------------
-	private static Properties props;
-	public static enum Type    { REPLAY, IVY }
-	public static enum MapType { MAPNIK, BING_AERIAL, OSM_CYCLE_MAP }
-	//-----------------------------------------------------------------------------
-	// FIXME en attendant mieux
-	public static boolean TUIO = true;
-	public static boolean FULLSCREEN = false;
-	//-----------------------------------------------------------------------------
-	static
-	{
-		// Lecure de la clé dans le fichier "StaticMapsAPIKey.properties" (attribut "key")
-		props = new Properties();
-		try { props.load(new FileInputStream("uav3i.properties")); }
-		catch (IOException e) { e.printStackTrace(); }
-	}
-	//-----------------------------------------------------------------------------
-	public static Type getType()
-	{
-		String type = props.getProperty("TYPE");
-		if (type.equalsIgnoreCase("replay"))
-			return Type.REPLAY;
-		else if (type.equalsIgnoreCase("ivy"))
-			return Type.IVY;
-		else
-			return null;
-	}
-	//-----------------------------------------------------------------------------
-	public static MapType getMapType()
-	{
-		String type = props.getProperty("MAP_TYPE");
-		if (type.equalsIgnoreCase("mapnik"))
-			return MapType.MAPNIK;
-		else if (type.equalsIgnoreCase("bing_aerial"))
-			return MapType.BING_AERIAL;
-		else if (type.equalsIgnoreCase("osm_cycle_map"))
-			return MapType.OSM_CYCLE_MAP;
-		else
-			return null;
-	}
-	//-----------------------------------------------------------------------------
-	public static int getInitialZoom()      { return Integer.parseInt(props.getProperty("INITIAL_ZOOM"));      }
-	public static int getInitialLatitude()  { return Integer.parseInt(props.getProperty("INITIAL_LATITUDE"));  }
-	public static int getInitialLongitude() { return Integer.parseInt(props.getProperty("INITIAL_LONGITUDE")); }
-	//-----------------------------------------------------------------------------
+  //-----------------------------------------------------------------------------
+  private static Properties props;
+  public static enum Mode    { REPLAY, IVY }
+  public static enum MapType { MAPNIK, BING_AERIAL, OSM_CYCLE_MAP }
+  //-----------------------------------------------------------------------------
+  static
+  {
+    props = new Properties();
+    try { props.load(new FileInputStream("uav3i.properties")); }
+    catch (IOException e) { e.printStackTrace(); }
+  }
+  //-----------------------------------------------------------------------------
+  public static Mode getMode()
+  {
+    String type = props.getProperty("MODE");
+    if (type.equalsIgnoreCase("replay"))
+      return Mode.REPLAY;
+    else if (type.equalsIgnoreCase("ivy"))
+      return Mode.IVY;
+    else
+      return null;
+  }
+  //-----------------------------------------------------------------------------
+  public static MapType getMapType()
+  {
+    String type = props.getProperty("MAP_TYPE");
+    if (type.equalsIgnoreCase("mapnik"))
+      return MapType.MAPNIK;
+    else if (type.equalsIgnoreCase("bing_aerial"))
+      return MapType.BING_AERIAL;
+    else if (type.equalsIgnoreCase("osm_cycle_map"))
+      return MapType.OSM_CYCLE_MAP;
+    else
+      return null;
+  }
+  //-----------------------------------------------------------------------------
+  public static int     getInitialZoom()      { return Integer.parseInt(props.getProperty("INITIAL_ZOOM"));        }
+  public static int     getTrajectoryZoom()   { return Integer.parseInt(props.getProperty("TRAJECTORY_ZOOM"));     }
+  public static double  getInitialLatitude()  { return Double.parseDouble(props.getProperty("INITIAL_LATITUDE"));  }
+  public static double  getInitialLongitude() { return Double.parseDouble(props.getProperty("INITIAL_LONGITUDE")); }
+  public static boolean getTUIO()             { return Boolean.parseBoolean(props.getProperty("TUIO"));            }
+  public static boolean getFullscreen()       { return Boolean.parseBoolean(props.getProperty("FULLSCREEN"));      }
+  //-----------------------------------------------------------------------------
 }
