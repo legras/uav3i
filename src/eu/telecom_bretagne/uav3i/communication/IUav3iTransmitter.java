c44fd2bb66029b15eca5c4db93084116213713e8
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2013-10-31 09:48:37 +0100

    Uav3iTransmitter.

---
+++ src/eu/telecom_bretagne/uav3i/communication/IUav3iTransmitter.java
@@ -0,0 +1,27 @@
+package eu.telecom_bretagne.uav3i.communication;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface IUav3iTransmitter extends Remote
+{
+  //-----------------------------------------------------------------------------
+  /**
+   * Ajout d'une nouvelle position pour le drone.
+   * 
+   * @param utm_east coordonnée UTM Est
+   * @param utm_north coordonnée UTM Nord
+   * @param utm_zone fuseau UTM
+   * @param course cap du drone
+   * @param alt altitude
+   * @param t temps
+   * @throws RemoteException
+   */
+  public void addUAVDataPoint(int  utm_east,
+                              int  utm_north,
+                              int  utm_zone,
+                              int  course,
+                              int  alt,
+                              long t) throws RemoteException;
+  //-----------------------------------------------------------------------------
+}
---
+++ src/eu/telecom_bretagne/uav3i/communication/Uav3iTransmitterImpl.java
@@ -0,0 +1,21 @@
+package eu.telecom_bretagne.uav3i.communication;
+
+import java.rmi.RemoteException;
+
+import com.deev.interaction.uav3i.model.UAVDataStore;
+
+public class Uav3iTransmitterImpl implements IUav3iTransmitter
+{
+  //-----------------------------------------------------------------------------
+  @Override
+  public void addUAVDataPoint(int  utm_east,
+                              int  utm_north, 
+                              int  utm_zone, 
+                              int  course, 
+                              int  alt, 
+                              long t) throws RemoteException
+  {
+    UAVDataStore.addUAVDataPoint(utm_east, utm_north, utm_zone, course, alt, t);
+  }
+  //-----------------------------------------------------------------------------
+}
