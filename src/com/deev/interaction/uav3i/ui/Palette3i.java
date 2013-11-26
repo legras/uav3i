3fb0b04fd3bdcbdfb3b6eedacc59a374f4775805
François Legras <francois.legras@deev-interaction.com>
2013-11-26 14:42:57 +0100

    Début Timeline 2

--- src/com/deev/interaction/uav3i/ui/Palette3i.java
+++ src/com/deev/interaction/uav3i/ui/Palette3i.java
@@ -13,7 +13,11 @@ import eu.telecom_bretagne.uav3i.util.log.LoggerUtil;
 public enum Palette3i 
 {	
 	BUTTON_WHITE_BG(1.f, 1.f, 1.f, .5f),
-	BUTTON_DISABLED_BG("img/stripesGray32.png", 16);
+	BUTTON_DISABLED_BG("img/stripesGray32.png", 16),
+	TIME_LIGHT(.9f, .9f, .9f, .5f),
+	TIME_LIGHTER(1.f, 1.f, 1.f, .5f),
+	TIME_DARK(.9f, .9f, .9f, .5f),
+	TIME_DARKER(.9f, .9f, .9f, .5f);
 	
 	private Paint _paint;
 	
