5810b82cfdd571f8b912778360b61f1f2baece05
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2014-04-01 18:21:42 +0200

    Les classes DTO avaient été oubliées...

---
+++ src/eu/telecom_bretagne/uav3i/communication/CircleMnvrDTO.java
@@ -0,0 +1,22 @@
+package eu.telecom_bretagne.uav3i.communication;
+
+import uk.me.jstott.jcoord.LatLng;
+
+public class CircleMnvrDTO extends ManoeuverDTO
+{
+  //-----------------------------------------------------------------------------
+  private static final long serialVersionUID = 8781703792280948005L;
+  //-----------------------------------------------------------------------------
+  private LatLng _center;
+  private double _currentRm = 500.;
+  //-----------------------------------------------------------------------------
+  public CircleMnvrDTO(LatLng center, double currentRm)
+  {
+    _center    = center;
+    _currentRm = currentRm;
+  }
+  //-----------------------------------------------------------------------------
+  public LatLng get_center()    { return _center;    }
+  public double get_currentRm() { return _currentRm; }
+  //-----------------------------------------------------------------------------
+}
---
+++ src/eu/telecom_bretagne/uav3i/communication/ManoeuverDTO.java
@@ -0,0 +1,10 @@
+package eu.telecom_bretagne.uav3i.communication;
+
+import java.io.Serializable;
+
+public class ManoeuverDTO implements Serializable
+{
+  //-----------------------------------------------------------------------------
+  private static final long serialVersionUID = -3496424950260659940L;
+  //-----------------------------------------------------------------------------
+}
