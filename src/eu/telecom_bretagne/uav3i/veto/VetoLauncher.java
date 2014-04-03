038f811613c00f66e0c6f8b0505bc1b357a7ad65
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2014-04-03 13:40:57 +0200

    Création classe VetoLauncher (IHM).

---
+++ src/eu/telecom_bretagne/uav3i/veto/VetoLauncher.java
@@ -0,0 +1,10 @@
+package eu.telecom_bretagne.uav3i.veto;
+
+public class VetoLauncher
+{
+  //-----------------------------------------------------------------------------
+  public static void main(String[] args)
+  {
+  }
+  //-----------------------------------------------------------------------------
+}
