10ff989d4f39cffa28c171d9069c9b36f49905da
Freek van Tienen <freek.v.tienen@gmail.com>
2015-04-15 09:56:25 +0200

    Gradle setup

--- uav3i.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp ./bin:lib/gnu.getopt-1.0.9.jar:lib/ivy-java-1.2.6.jar:lib/jakarta-regexp-1.5.jar:lib/libTUIO.jar:lib/commons-math3-3.2.jar:lib/JMapViewer.jar:lib/Serializable_jcoord-1.0.jar -Xms512m -Xmx1024m com.deev.interaction.uav3i.ui.Launcher
