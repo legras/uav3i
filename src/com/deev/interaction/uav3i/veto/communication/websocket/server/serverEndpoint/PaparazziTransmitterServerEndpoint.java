090e979d9237a75fc5e19eaa5e4881bdf69d16b2
Philippe TANGUY <philippe.tanguy@telecom-bretagne.eu>
2015-06-12 17:32:34 +0200

    Command value is sent via the URL.

--- src/com/deev/interaction/uav3i/veto/communication/websocket/server/serverEndpoint/PaparazziTransmitterServerEndpoint.java
+++ src/com/deev/interaction/uav3i/veto/communication/websocket/server/serverEndpoint/PaparazziTransmitterServerEndpoint.java
@@ -1,20 +1,21 @@
 package com.deev.interaction.uav3i.veto.communication.websocket.server.serverEndpoint;
 
 import javax.websocket.OnMessage;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import com.deev.interaction.uav3i.veto.communication.dto.ManoeuverDTO;
 import com.deev.interaction.uav3i.veto.communication.dto.ManoeuverDTODecoder;
 import com.deev.interaction.uav3i.veto.communication.dto.ManoeuverDTOEncoder;
 
-@ServerEndpoint(value    = "/PaparazziTransmitter",
+@ServerEndpoint(value    = "/PaparazziTransmitter/{command}",
                 decoders = ManoeuverDTODecoder.class,
                 encoders = ManoeuverDTOEncoder.class)
 public class PaparazziTransmitterServerEndpoint
 {
   //-----------------------------------------------------------------------------
   @OnMessage
-  public void receive(String command, ManoeuverDTO mnvrDTO)
+  public void receive(@PathParam("command") String command, ManoeuverDTO mnvrDTO)
   {
     System.out.println("####### PaparazziTransmitterServerEndpoint.receive(" + command + ", " + mnvrDTO + ")");
     switch (command)
