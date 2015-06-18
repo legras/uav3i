f14205f35514b9fab2a650d737a42e146f08eece
ptanguy <philippe.tanguy@telecom-bretagne.eu>
2015-06-18 18:54:16 +0200

    PaparazziTransmitterExecuteServerEndpoint to be tested.

--- src/com/deev/interaction/uav3i/veto/communication/websocket/serverEndpoint/PaparazziTransmitterExecuteServerEndpoint.java
+++ src/com/deev/interaction/uav3i/veto/communication/websocket/serverEndpoint/PaparazziTransmitterExecuteServerEndpoint.java
@@ -11,7 +11,16 @@ import javax.websocket.OnMessage;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 
+import com.deev.interaction.uav3i.util.UAV3iSettings;
+import com.deev.interaction.uav3i.util.UAV3iSettings.Mode;
+import com.deev.interaction.uav3i.util.UAV3iSettings.VetoMode;
 import com.deev.interaction.uav3i.util.log.LoggerUtil;
+import com.deev.interaction.uav3i.veto.communication.dto.ManoeuverDTO;
+import com.deev.interaction.uav3i.veto.communication.dto.ManoeuverDTO.ManoeuverRequestedStatus;
+import com.deev.interaction.uav3i.veto.communication.websocket.PaparazziTransmitterWebsocket;
+import com.deev.interaction.uav3i.veto.ui.Veto;
+
+import fr.dgac.ivy.IvyException;
 
 @ServerEndpoint(value = "/PaparazziTransmitterExecute")
 public class PaparazziTransmitterExecuteServerEndpoint
@@ -21,41 +30,49 @@ public class PaparazziTransmitterExecuteServerEndpoint
   public void receive(String idManoeuver)
   {
     System.out.println("####### PaparazziTransmitterExecuteServerEndpoint.receive("+idManoeuver+")");
-//    int idMnvr = Integer.parseInt(idManoeuver)
-//    // Comme les boutons ne sont pas 'Serializable' (et qu'on n'en a rien à
-//    // faire côté table), on les ajoute sur la manoeuvre une fois qu'elle
-//    // arrive sur le Veto. Comme cette manoeuvre (même si c'est la même) est une
-//    // copie, on récupère celle précédemment transmise lors du 'communicateManoeuver'
-//    // pour lui ajouter les boutons.
-//    // TODO : il n'est pas utile de transmettre l'intance de la manoeuvre, seul son id est suffisant.
-//    ManoeuverDTO mDTO = Veto.getSymbolMapVeto().getSharedManoeuver();  
-//    if(mDTO != null)
-//    {
-//      if(mDTO.getId() == idMnvr)
-//      {
-//        if(UAV3iSettings.getMode() == Mode.VETO)
-//        {
-//          LoggerUtil.LOG.info("executeManoeuver("+mDTO+") asked");
-//          mDTO.addButtons();
-//          mDTO.setRequestedStatus(ManoeuverRequestedStatus.ASKED);
-//        }
-//        else if(UAV3iSettings.getMode() == Mode.VETO_AUTO)
-//        {
-//          LoggerUtil.LOG.info("executeManoeuver("+mDTO+") automaticaly accepted");
-//          // On transmet à la table le résultat de l'évaluation de la manoeuvre
-//          // par l'opérateur Paparazzi pour mise à jour de l'affichage.
-//          instance.getUav3iTransmitter().resultAskExecution(idMnvr, true);
-//          // On met à jour localement le statut de la manoeuvre pour mise
-//          // à jour de l'affichage sur le Veto.
-//          mDTO.setRequestedStatus(ManoeuverRequestedStatus.ACCEPTED);
-//          // On lance l'exécution de la manoeuvre.
-//          instance.startManoeuver(mDTO);
-//        }
-//      }
-//    }
-//    else
-//      LoggerUtil.LOG.severe(("Exection of a manoeuver that is not shared : " + idMnvr));
-//
+    int idMnvr = Integer.parseInt(idManoeuver);
+    // Comme les boutons ne sont pas 'Serializable' (et qu'on n'en a rien à
+    // faire côté table), on les ajoute sur la manoeuvre une fois qu'elle
+    // arrive sur le Veto. Comme cette manoeuvre (même si c'est la même) est une
+    // copie, on récupère celle précédemment transmise lors du 'communicateManoeuver'
+    // pour lui ajouter les boutons.
+    // TODO : il n'est pas utile de transmettre l'intance de la manoeuvre, seul son id est suffisant.
+    ManoeuverDTO mDTO = Veto.getSymbolMapVeto().getSharedManoeuver();  
+    if(mDTO != null)
+    {
+      if(mDTO.getId() == idMnvr)
+      {
+        if(UAV3iSettings.getVetoMode() == VetoMode.MANUEL)
+        {
+          LoggerUtil.LOG.info("executeManoeuver("+mDTO+") automaticaly accepted");
+          // On transmet à la table le résultat de l'évaluation de la manoeuvre
+          // par l'opérateur Paparazzi pour mise à jour de l'affichage.
+          Uav3iTransmitterResultAskExecutionServerEndpoint.resultAskExecution(idMnvr, true);
+          // On met à jour localement le statut de la manoeuvre pour mise
+          // à jour de l'affichage sur le Veto.
+          mDTO.setRequestedStatus(ManoeuverRequestedStatus.ACCEPTED);
+          // On lance l'exécution de la manoeuvre.
+          try
+          {
+            PaparazziTransmitterWebsocket.getInstance().startManoeuver(mDTO);
+          }
+          catch (IvyException e)
+          {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+          }
+        }
+        else if(UAV3iSettings.getVetoMode() == VetoMode.AUTOMATIC)
+        {
+          LoggerUtil.LOG.info("executeManoeuver("+mDTO+") asked");
+          mDTO.addButtons();
+          mDTO.setRequestedStatus(ManoeuverRequestedStatus.ASKED);
+        }
+      }
+    }
+    else
+      LoggerUtil.LOG.severe(("Exection of a manoeuver that is not shared : " + idMnvr));
+
   }
   //-----------------------------------------------------------------------------
   @OnClose
